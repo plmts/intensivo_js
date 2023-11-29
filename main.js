26c53b485dd2f8f2408ab7af968ca683ca474495
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-11-29 13:43:13 -0300

    adicionado estilização básica à página. Alteradas as cores bases da webpage e adicionado css à alguns botões.

--- main.js
+++ main.js
@@ -5,30 +5,30 @@ import { inicializarCarrinho } from "./src/menuCarrinho"
 const catalogo = [
   {
     id: 1,
-    nome: 'Reboque simples',
+    nome: 'Reboque Simples',
     preco: 150,
     nomeArquivoImagem: 'reboque.jpg'
   },
   {
     id:2,
-    nome: 'Baú aberto',
+    nome: 'Baú Aberto',
     preco: 400,
     nomeArquivoImagem: 'bau_aberto.jpg'
   },
   {
     id:3,
-    nome: 'Baú fechado',
+    nome: 'Baú Fechado',
     preco: 600,
     nomeArquivoImagem: 'bau_fechado.jpg'
   },
 ]
 
 for (const produtoCatalogo of catalogo) {
-  const cartaoProduto =    ` <div class="border-double border-4 border-sky-500 w-96 m-2 p-2 id="card-produto-${produtoCatalogo.id}">
-  <img src="/img/${produtoCatalogo.nomeArquivoImagem}" alt="reboque" style="height: 300px;">
-  <p class="nome">${produtoCatalogo.nome}</p>
-  <p>Valor: R$ ${produtoCatalogo.preco}</p>
-  <button>Entrar em contato</button>
+  const cartaoProduto =    ` <div class="rounded-lg border-4 border-sky-500 w-96 m-3 mx-8 p-2 id="card-produto-${produtoCatalogo.id}">
+  <img class="rounded-full" src="/img/${produtoCatalogo.nomeArquivoImagem}" alt="${produtoCatalogo.nome}" style="height: 300px;">
+  <p class="nome text-red-400">${produtoCatalogo.nome}</p>
+  <p class="text-amber-200">Valor: R$ ${produtoCatalogo.preco}</p>
+  <button class="bg-transparent hover:bg-amber-200 text-lime-400 font-semibold hover:text-white py-2 px-4 border border-sky-500 hover:border-transparent rounded">Entrar em contato</button>
   <br><br>
   </div>`
   
