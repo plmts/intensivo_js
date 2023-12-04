b251af7c61c30d2c86d8aeaee8c1a7c8dc67d81b
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-12-04 15:43:45 -0300

    corrigindo articles do carrinho, pois botões de aumentar e diminuir só funcionavam no último elemento adicionado.

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -41,12 +41,17 @@ export function adicionarAoCarrinho(idProduto){
       incrementarQuantidadeProduto(idProduto);
       return
     }
-
   idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+
   const  produto = catalogo.find((p) => p.id === idProduto);
   const conteinerProdutoCarrinho = document.getElementById("produtos-carrinho");
-  const cartaoProdutoCarrinho = `<article class="bg-slate-200 text-black rounded-lg p-1">
-  <button class="text-red-500 hover:bg-red-400 hover:text-slate-200 rounded-full" id="fechar-carrinho"><i class="fa-solid fa-xmark"></i></button>
+
+  const elementoArticle = document.createElement("article");
+  const articleClasses = ['bg-slate-200', 'text-black', 'rounded-lg', 'p-1']
+  for (const articleClass of articleClasses){
+    elementoArticle.classList.add(articleClass);
+  }
+  const cartaoProdutoCarrinho = `<button class="text-red-500 hover:bg-red-400 hover:text-slate-200 rounded-full" id="fechar-carrinho"><i class="fa-solid fa-xmark"></i></button>
   <div class="flex justify-between" id="card-1">
   <div>
   <img src="./img/${produto.nomeArquivoImagem}" alt="carrinho:${produto.nome}" class="h-20 rounded-full">
@@ -58,9 +63,9 @@ export function adicionarAoCarrinho(idProduto){
     <p id='quantidade${produto.id}'>${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
     <button id='decrementar-produto-${produto.id}' class="bg-red-200 text-black rounded-full"><i class="fa-solid fa-minus"></i></button>
   </div>          
-  </div>
-  </article><br>`; 
-  conteinerProdutoCarrinho.innerHTML += cartaoProdutoCarrinho;
+  </div><br>`; 
+  elementoArticle.innerHTML = cartaoProdutoCarrinho;
+  conteinerProdutoCarrinho.appendChild(elementoArticle)
 
   document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click',() => decrementarQuantidadeProduto(produto.id));
   document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click',() => incrementarQuantidadeProduto(produto.id));
