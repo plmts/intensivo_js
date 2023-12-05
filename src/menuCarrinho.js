8a2dc55bbcf0033184377067ca5c8fdafc3762e7
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-12-05 15:48:06 -0300

    adicionando inteligência ao botão para excluir os itens do carrinho, além de evitar que a contagem dos itens fique negativa.

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,19 +1,23 @@
 //Importações
 import { catalogo } from "./utilidades";
 
-const idsProdutoCarrinhoComQuantidade = {}
+const idsProdutoCarrinhoComQuantidade = {};
+
 
 //Funções
+//ABRIR CARRINHO
 function abrirCarrinho() {
   document.getElementById("carrinho-frete").classList.add("right-[0px]");
   document.getElementById("carrinho-frete").classList.remove("right-[360px]");
 }
 
+//FECHAR CARRINHO
 function fecharCarrinho() {
   document.getElementById("carrinho-frete").classList.remove("right-[0px]");
   document.getElementById("carrinho-frete").classList.add("right-[-360px]");
 }
 
+//INICIALIZAR CARRINHO
 export function inicializarCarrinho() {
   const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
   const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
@@ -22,28 +26,34 @@ export function inicializarCarrinho() {
   botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
-function renderizarProdutosCarrinho() {
-  
-}
-
+//REMOVER DO CARRINHO
 function removerDoCarrinho(idProduto){
-  delete idsProdutoCarrinhoComQuantidade(idProduto);
+  delete idsProdutoCarrinhoComQuantidade[idProduto];
+  renderizarProdutosCarrinho();
 }
 
+//INCREMENTAR QUANTIDADE DOS PRODUTOS
 function incrementarQuantidadeProduto(idProduto){
   idsProdutoCarrinhoComQuantidade[idProduto]++;
   atualizarInformacaoQuantidade(idProduto);
 }
 
+//DECREMENTAR QUANTIDADE DOS PRODUTOS
 function decrementarQuantidadeProduto(idProduto){
+  if (idsProdutoCarrinhoComQuantidade[idProduto] === 1) {
+    removerDoCarrinho(idProduto);
+    return;
+  }
   idsProdutoCarrinhoComQuantidade[idProduto]--;
   atualizarInformacaoQuantidade(idProduto)
 }
 
+//ATUALIZAR INFORMAÇÃO DAS QUANTIDADES
 function atualizarInformacaoQuantidade(idProduto){
   document.getElementById(`quantidade${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
 }
 
+//DESENHAR CARRINHO
 function desenharProdutoCarrinho(idProduto){
   const  produto = catalogo.find((p) => p.id === idProduto);
   const conteinerProdutoCarrinho = document.getElementById("produtos-carrinho");
@@ -53,7 +63,7 @@ function desenharProdutoCarrinho(idProduto){
   for (const articleClass of articleClasses){
     elementoArticle.classList.add(articleClass);
   }
-  const cartaoProdutoCarrinho = `<button class="text-red-500 hover:bg-red-400 hover:text-slate-200 rounded-full" id="fechar-carrinho"><i class="fa-solid fa-xmark"></i></button>
+  const cartaoProdutoCarrinho = `<button class="text-red-500 hover:bg-red-400 hover:text-slate-200 rounded-full" id="remover-item-${produto.id}"><i class="fa-solid fa-xmark"></i></button>
   <div class="flex justify-between" id="card-1">
   <div>
   <img src="./img/${produto.nomeArquivoImagem}" alt="carrinho:${produto.nome}" class="h-20 rounded-full">
@@ -71,13 +81,24 @@ function desenharProdutoCarrinho(idProduto){
 
   document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click',() => decrementarQuantidadeProduto(produto.id));
   document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click',() => incrementarQuantidadeProduto(produto.id));
+  document.getElementById(`remover-item-${produto.id}`).addEventListener('click',() => removerDoCarrinho(produto.id));
 }
 
+//RENDERIZAR PRODUTO NO CARRINHO
+function renderizarProdutosCarrinho() {
+  const conteinerProdutoCarrinho = document.getElementById("produtos-carrinho");
+  conteinerProdutoCarrinho.innerHTML = "";
+  for (const idProduto in idsProdutoCarrinhoComQuantidade){
+    desenharProdutoCarrinho(idProduto);
+  }
+}
+
+// ADICIONAR PRODUTO AO CARRINHO
 export function adicionarAoCarrinho(idProduto){
-    if (idProduto in idsProdutoCarrinhoComQuantidade){
-      incrementarQuantidadeProduto(idProduto);
-      return
-    }
+  if (idProduto in idsProdutoCarrinhoComQuantidade){
+    incrementarQuantidadeProduto(idProduto);
+    return
+  }
   idsProdutoCarrinhoComQuantidade[idProduto] = 1;
   desenharProdutoCarrinho(idProduto);
 }
